ca4261a0ee781353920dda87c23884f277265c37
annusingmar <108177565+annusingmar@users.noreply.github.com>
2022-08-22 21:28:35 +0300

    assignment display pt2

--- src/components/StudentAssignmentListCard.vue
+++ src/components/StudentAssignmentListCard.vue
@@ -3,19 +3,33 @@
     <q-card-section class="q-pb-none">
       <div class="text-h6">Assignments</div>
       <div class="row justify-end items-center">
-        <q-btn dense size="sm" label="show older" @click="showOlder"></q-btn>
+        <q-btn
+          v-if="showOlderButton"
+          dense
+          size="sm"
+          label="show older"
+          @click="showOlder"
+        ></q-btn>
+        <div v-else>No more to show.</div>
       </div>
     </q-card-section>
     <q-card-section class="q-gutter-y-sm">
       <StudentAssignmentListDayItem
-        v-for="(item, key) in assignments"
+        v-for="[key, val] in sortedAssignments"
         :id="id"
         :key="key"
         :date="key"
-        :assignments="item"
+        :assignments="val"
       />
       <div class="row justify-end items-center">
-        <q-btn dense size="sm" label="show future" @click="showFuture"></q-btn>
+        <q-btn
+          v-if="showMoreButton"
+          dense
+          size="sm"
+          label="show more"
+          @click="showMore"
+        ></q-btn>
+        <div v-else>No more to show.</div>
       </div>
     </q-card-section>
     <q-inner-loading :showing="loading"></q-inner-loading>
@@ -25,53 +39,84 @@
 <script setup>
 import { api } from "src/boot/axios";
 import { useUserStore } from "src/stores/user";
-import { reactive, ref } from "vue";
+import { computed, ref } from "vue";
 import { useQuasar, date } from "quasar";
 import StudentAssignmentListDayItem from "./StudentAssignmentListDayItem.vue";
 
 const $q = useQuasar();
 const { id } = useUserStore();
 
-const startDate = ref(new Date());
-const endDate = ref(date.addToDate(startDate.value, { days: 5 }));
+// i got no clue whether or not this is efficient
+// but it's the best i could come up with
+const sortedAssignments = computed(
+  () =>
+    new Map(
+      Array.from(assignments.value).sort((a, b) =>
+        date.getDateDiff(new Date(a[0]), new Date(b[0]), "date")
+      )
+    )
+);
 
-const showOlder = () => {
-  startDate.value = date.subtractFromDate(startDate.value, { days: 5 });
-  getAssignments();
+const showOlderButton = ref(true);
+const showMoreButton = ref(true);
+
+const showOlder = async () => {
+  const [f] = sortedAssignments.value.keys();
+  const datesAdded = await getAssignments(
+    "desc",
+    date.subtractFromDate(new Date(f), { days: 1 })
+  );
+  if (datesAdded < 4) {
+    showOlderButton.value = false;
+  }
 };
 
-const showFuture = () => {
-  endDate.value = date.addToDate(endDate.value, { days: 5 });
-  getAssignments();
+const showMore = async () => {
+  const datesAdded = await getAssignments(
+    "asc",
+    date.addToDate(new Date(Array.from(sortedAssignments.value.keys()).pop()), {
+      days: 1,
+    })
+  );
+  if (datesAdded < 4) {
+    showMoreButton.value = false;
+  }
 };
 
 const loading = ref(true);
-const assignments = ref({});
-const getAssignments = async () => {
+const assignments = ref(new Map());
+const getAssignments = async (direction = "asc", startDate = new Date()) => {
   loading.value = true;
-  assignments.value = {};
+  let datesAdded = 0;
   try {
     const response = await api.get("/students/" + id + "/assignments", {
       params: {
-        start_date: date.formatDate(startDate.value, "YYYY-MM-DD"),
-        end_date: date.formatDate(endDate.value, "YYYY-MM-DD"),
+        start_date: date.formatDate(startDate, "YYYY-MM-DD"),
+        direction,
+        limit: 4,
       },
     });
-    for (
-      let c = new Date(startDate.value);
-      c <= endDate.value;
-      c = date.addToDate(c, { days: 1 })
-    ) {
-      assignments.value[date.formatDate(c, "YYYY-MM-DD")] = [];
-    }
     if (response.data.assignments) {
-      response.data.assignments.forEach((a) =>
-        assignments.value[a.deadline].push(a)
-      );
+      response.data.assignments.forEach((a) => {
+        let date = assignments.value.get(a.deadline);
+        if (date === undefined) {
+          datesAdded += 1;
+          date = [];
+          assignments.value.set(a.deadline, date);
+        }
+        date.push(a);
+      });
     }
     loading.value = false;
+    return datesAdded;
   } catch (error) {
-    console.log(error);
+    $q.notify({
+      type: "negative",
+      position: "top",
+      message: "Loading data failed",
+      timeout: 0,
+      actions: [{ label: "Dismiss", color: "white" }],
+    });
   }
 };
 
