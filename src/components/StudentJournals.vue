a306e72f0d624806b91a6408287a8db82a6c4ebe
annusingmar <108177565+annusingmar@users.noreply.github.com>
2023-02-22 20:35:04 +0200

    show class year name

--- src/components/StudentJournals.vue
+++ src/components/StudentJournals.vue
@@ -11,7 +11,12 @@
             dense
             dense-options
             option-value="id"
-            option-label="display_name"
+            :option-label="
+              (item) =>
+                item.class_name
+                  ? `${item.display_name} (${item.class_name})`
+                  : item.display_name
+            "
           ></q-select>
         </q-card-section>
         <q-card-section>
